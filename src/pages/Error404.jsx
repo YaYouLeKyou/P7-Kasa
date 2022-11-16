a54e24770b7d69758017c0210daddc56c4b5b9dc
Yayou <yanes75@hotmail.fr>
2022-11-16 10:24:56 +0100

    Error404

--- src/pages/Error404.jsx
+++ src/pages/Error404.jsx
@@ -1,11 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 const Error404 = () => {
   return (
-    <div>
-      Error404
-    </div>
+    <main className="ErrorMain">
+      <div className="ErrorTitle">
+        <h1>404</h1>
+        <h2>Oups! La page que vous demandez n'existe pas.</h2>
+      </div>
+      <div className='LinkHome'>
+        <NavLink to={'/home'} style={{ textDecoration: 'underline' }}>
+          Retourner Sur la page d'accueil
+        </NavLink>
+      </div>
+    </main>
   );
-}
+};
 
-export default Error404
+export default Error404;
